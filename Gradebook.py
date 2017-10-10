c32f1100935704264f992e194040b20e3d20de37
matthewharrilal <matthewharrilal@gmail.com>
2017-10-09 21:34:41 -0700

    The roster for the courses has almost been implemented

--- Gradebook.py
+++ Gradebook.py
@@ -6,9 +6,10 @@ import uuid
 class Class():
     # This is going to be the class function
     possible_courses = ['Mathematics', 'Science', 'English', 'Economics', 'Social Studies', 'Geology']
+    random_course = random.choice(self.possible_courses)
     def courses(self):
         # This assigns the courses to the
-        print('Hello new student this is %s' %(random.choice(self.possible_courses)))
+        print('Hello new student this is %s' %(random_course))
 
     def times_courses_meet(self):
         # This is essentially the function that will determine when the courses meet
@@ -18,3 +19,10 @@ class Class():
         # This is essentially the function that will give the course a course id
         for course in possible_courses:
             unique_identification = uuid.uuid4(course)
+
+    def course_roster(self):
+
+        # This is essentially the roster for the course that is chosen
+        # The keys will be the random course while the values
+        # are the students that populate the course
+        roster_dictionary = {random_course: }
