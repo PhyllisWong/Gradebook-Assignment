64a3f91da75a7a76eb7cefe615927321750a12cc
matthewharrilal <matthewharrilal@gmail.com>
2017-10-09 20:29:44 -0700

    The course function has been completed

--- Gradebook.py
+++ Gradebook.py
@@ -1,5 +1,10 @@
+import random
+
 class Class():
     # This is going to be the class function
-    def course_name(self):
-        # This is the function that will assign the course name
-        
+    def courses(self):
+        # This assigns the courses to the
+        possible_courses = ['Mathematics', 'Science', 'English', 'Economics', 'Social Studies', 'Geology']
+        print('Hello new student this is %s' %(random.choice(possible_courses)))
+
+    
