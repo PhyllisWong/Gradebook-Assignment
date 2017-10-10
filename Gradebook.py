6cff23b937b3950eae4ba1e678e9cc8e7ae91382
matthewharrilal <matthewharrilal@gmail.com>
2017-10-09 20:57:03 -0700

    The time that the courses meet has been implemented

--- Gradebook.py
+++ Gradebook.py
@@ -1,10 +1,14 @@
 import random
+import datetime
+import time
 
 class Class():
     # This is going to be the class function
+    possible_courses = ['Mathematics', 'Science', 'English', 'Economics', 'Social Studies', 'Geology']
     def courses(self):
         # This assigns the courses to the
-        possible_courses = ['Mathematics', 'Science', 'English', 'Economics', 'Social Studies', 'Geology']
-        print('Hello new student this is %s' %(random.choice(possible_courses)))
+        print('Hello new student this is %s' %(random.choice(self.possible_courses)))
 
-    
+    def times_courses_meet(self):
+        # This is essentially the function that will determine when the courses meet
+        print('This course meets %s every Monday, Tuesday, and Friday' %(time.strftime('%H:%M:%S')))
