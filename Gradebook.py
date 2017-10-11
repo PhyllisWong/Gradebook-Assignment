a977f8c1c4c0a45a682ed00d46beb8c4a5ba8fe5
matthewharrilal <matthewharrilal@gmail.com>
2017-10-10 21:45:19 -0700

    Most recent update to the gradebook project

--- Gradebook.py
+++ Gradebook.py
@@ -2,15 +2,20 @@ import random
 import datetime
 import time
 import uuid
+from Students import Students
 
 class Classroom():
     # This is going to be the class function
     possible_courses = ['Mathematics', 'Science', 'English', 'Economics', 'Social Studies', 'Geology']
     random_course = random.choice(possible_courses)
+    chosen_course = random_course
     list_of_assignments = []
+    class_roster = []
 
+    def __init__(self):
+        self.roster = []
     def courses(self):
-        # This assigns the courses to the
+        # This essentially tells the students what the courses will be
         print('Hello new student this is %s' %(random_course))
 
     def times_courses_meet(self):
@@ -24,25 +29,24 @@ class Classroom():
             unique_identification = uuid.uuid4(course)
 
     def course_roster(self):
-
-        # This is essentially the roster for the course that is chosen
-        # The keys will be the random course while the values
-        # are the students that populate the course
-
-
-        # for course in possible_courses:
-        #     roster_dictionary = {course: 'students'}
-        pass
+        # This will essentially be the roster for the class and the students that populate the class
+        action = str(input)
+        if action == 'create':
+            print('What is the name of the student that you would like to add to the roster?')
+            student
 
     def assignments(self):
         # Handles the assignment functionality such as assigning updating and deleting
+        print("What do you want to do to assignments?")
         action = str(input)
         if action == 'create':
+            # This essentially creates the assignment
             print('What is the name of the assignment?')
             assignment_name = input()
             course_assignemnts = self.list_of_assignments.append(assignment_name)
             return(course_assignemnts)
         if action == 'delete':
+            # This essentially deletes an assignment
             assignment_name = input()
             if assignment_name not in self.list_of_assignments:
                 print('The assignment you are trying to delete does not exist')
@@ -50,6 +54,7 @@ class Classroom():
             else:
                 del self.list_of_assignments[assignment_name]
         if action == 'edit':
+            # This essentially edits or updates an assignment
             print('Which assignment are you trying to edit?')
             original_assignment = input()
             edited_assignment = input()
@@ -59,3 +64,19 @@ class Classroom():
             else:
                 new_assignment_name = self.list_of_assignments[original_assignment] = edited_assignment
                 return(new_assignment_name)
+
+    def create_student(self, student_name):
+        # Now that the students are essentially being added to the roster
+        new_student = Students(student)
+        self.roster.append(new_student)
+        return(roster)
+
+    def class_roster(self):
+        roster_dictionary = {}
+        for student in self.roster:
+            roster_dictionary[random_course] = student
+            # This essentially creates the roster dictionary
+
+
+# if __name__ == '__main__':
+#     # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
---
+++ Students.py
@@ -0,0 +1,4 @@
+class Students():
+    def __init__(self, name):
+        # So essentially every instance of this class will have a name of the student and from there we will append it to an array
+        self.name = name
