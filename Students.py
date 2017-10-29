cf113676c6363b1a66dd5ae78a9bfab82779c32f
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-28 20:24:10 -0700

    fixed typo in comments, added array for student courses.

--- Students.py
+++ Students.py
@@ -1,4 +1,7 @@
 class Students():
     def __init__(self, name):
-        # So essentially every instance of this class will have a name of the student and from there we will append it to an array
+        # SEvery instance of this class will have a name of the student
+        # and from there we will append it to an array
         self.name = name
+        # Every student has an array of courses
+        self.courses = []
